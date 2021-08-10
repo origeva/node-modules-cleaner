4cadb4b0fd37caca5bf1dfdeb407f0e22cd9bcdf
Ori Geva <origeva636@gmail.com>
2021-08-10 12:35:26 +0300

    Switch from deprecated fs.rmdir() function to fs.rm()

--- bin/nmc.js
+++ bin/nmc.js
@@ -1,47 +1,47 @@
 #! /usr/bin/env node
 
-const fs = require('fs/promises');
-const path = require('path');
+const fs = require('fs/promises')
+const path = require('path')
 
 const search = async (name, base, options) => {
-	base = base ? path.normalize(base) : process.cwd();
-	let dir = await fs.readdir(base);
-	let results = [];
+	base = base ? path.normalize(base) : process.cwd()
+	let dir = await fs.readdir(base)
+	let results = []
 	for (let i = 0; i < dir.length; i++) {
-		let currentPath = path.join(base, dir[i]);
+		let currentPath = path.join(base, dir[i])
 		if ((await fs.stat(currentPath)).isDirectory()) {
 			if (dir[i] === name) {
-				results.push(currentPath);
+				results.push(currentPath)
 			} else {
-				results.push(...(await search(name, currentPath, options)));
+				results.push(...(await search(name, currentPath, options)))
 			}
 		}
 	}
 
-	return results;
-};
+	return results
+}
 
 const clean = (dir) => {
-	return fs.rmdir(dir, { recursive: true });
-};
+	return fs.rm(dir, { recursive: true })
+}
 const nmc = () => {
-	let options = {};
-	let args = process.argv.slice(2);
-	if (args.includes('-l') || args.includes('--log')) options.log = true;
+	let options = {}
+	let args = process.argv.slice(2)
+	if (args.includes('-l') || args.includes('--log')) options.log = true
 
-	if (options.log) console.log('\x1b[32m', 'Searching...', '\x1b[0m');
+	if (options.log) console.log('\x1b[32m', 'Searching...', '\x1b[0m')
 	search('node_modules').then((results) => {
 		if (results.length) {
-			if (options.log) console.log('Directories found:', results);
+			if (options.log) console.log('Directories found:', results)
 			results.forEach((res) => {
-				clean(res);
-			});
+				clean(res)
+			})
 		} else {
-			console.error('\x1b[31m', 'No node_modules directories found', '\x1b[0m');
-			process.exit(1);
+			console.error('\x1b[31m', 'No node_modules directories found', '\x1b[0m')
+			process.exit(1)
 		}
-		console.log('\x1b[32m', 'nmc done!', '\x1b[0m');
-	});
-};
+		console.log('\x1b[32m', 'nmc done!', '\x1b[0m')
+	})
+}
 
-nmc();
+nmc()
